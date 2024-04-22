1becf144b7a72cb7fd55855ef47a467800adf144
BarbaraCrLp <barbaracrlp99@gmail.com>
2024-04-22 09:44:37 +0200

    voy a dedicarme ahora a hacer un mocking de la APi, asi no tengo que de momento pelearme con el token

--- app/Filament/Resources/PedidoResource/Pages/EditPedido.php
+++ app/Filament/Resources/PedidoResource/Pages/EditPedido.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\PedidoResource\Pages;
 use App\Filament\Resources\PedidoResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Http;
 
 use function Laravel\Prompts\alert;
 
@@ -25,6 +26,16 @@ class EditPedido extends EditRecord
         //aqui como tal va la llamada http 
         //para poder sacar el token de auth para el api, necesitare cambiar de alguna manera el login no sé como
    //si qeu funciona y redirije a la funcion que toca
+   //necesite la $data, té que estar guardat
+   $url='aqui va la url del api"';
+   //https:// Domain/gestion/api/tipollamada
+   //el tipo de llamada en teoría es un submit
+
+   //lo que necesitoe s como sacar el token de autentificacio per a poder conectarme al api
+   //volver a mirar lo de conectar el api con lo de ronda solo para poner el API token 
+   //luego hacer la llamada con este medoto? no sé si funcionara la verdad
+   //vuelvo a mirar lo del api
+   
     }
 
 }
