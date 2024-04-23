69985e15a6ba3fd0649b7c400cd7d7bbc559cb30
BarbaraCrLp <barbaracrlp99@gmail.com>
2024-04-23 12:17:15 +0200

    creo la nueva funcion que no devuelve nada, procedo a cambiarla para que devuelva un booleano

--- app/Models/Token.php
+++ app/Models/Token.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+//importo lo que es la DB
+use Illuminate\Support\Facades\DB;
 
 class Token extends Model
 {
@@ -27,4 +31,30 @@ class Token extends Model
     public function user():BelongsTo{
         return $this->belongsTo(User::class);
     }
+
+    /**Primera funcion para crear un token  */
+    public function newToken():void
+    {   
+
+        //creo el token como tal
+        $token= md5(uniqid('dinacms_token',true));
+        //creo la fecha de caducidad
+        $caducity=date('Y-m-d H:i:s',strtotime('+24 hours'));
+         try{
+
+            $insert=DB::table('auth_token')->insert([
+                'userid'=>4,
+                'token'=>$token,
+                'caducity'=>$caducity
+            ]);
+            if (!$insert) {
+                throw new Exception('Error inserting token into database.');
+            }
+         }
+         catch(Exception $e){
+            error_log('Error creating the new Token '.$e->getMessage());
+         }
+    }
+
+
 }
