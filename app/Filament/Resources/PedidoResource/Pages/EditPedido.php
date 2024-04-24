078053137619f38b0aa79a083f37c4f7272a4ecf
BarbaraCrLp <barbaracrlp99@gmail.com>
2024-04-24 13:47:28 +0200

    añado la funcion en teoria igual de la api voy a probar a ver si funciona y que me devuelve

--- app/Filament/Resources/PedidoResource/Pages/EditPedido.php
+++ app/Filament/Resources/PedidoResource/Pages/EditPedido.php
@@ -10,6 +10,7 @@ use Filament\Resources\Pages\EditRecord;
 use Illuminate\Support\Facades\Http;
 
 use function Laravel\Prompts\alert;
+use function Laravel\Prompts\error;
 
 class EditPedido extends EditRecord
 {
@@ -124,11 +125,60 @@ class EditPedido extends EditRecord
 
 
         error_log(json_encode($params));
+        $url_del_API='https://barbaratest01.arcadina.web2/gestion/api/ajaxsubmit.php';
 
-        
+        $respuesta=EditPedido::callApiHttp($url_del_API,$params,$timeout=5);
+        error_log(json_encode($respuesta));
+    }    //en el record tengo lo que son todos los datos de ese momento 
+    //record es la instancia de pedido que hay 
 
+public function callApiHttp(string $url, array $params, int $timeout = 5): array
+{
+    $ret = [
+        'resultado' => 'nok',
+        'mensaje' => '',
+    ];
+  
+    try {
+        $response = Http::timeout($timeout)->post($url, $params);
+      
+        $statusCode = $response->status();
+        $json = '';
+        $ret = null;
 
-        
+        if ($statusCode == 200) { // Si el status es OK
+            $json = $response->body(); // Intentamos recuperar el cuerpo de la respuesta
+            if ($json !== '') { // Si lo hemos podido recuperar
+                $tmp = json_decode($json, true); // Lo decodificamos desde JSON a un array
+                if ($tmp === null) {
+                    $lastError = "{$url} no devolvió JSON válido";
+                } elseif (is_array($tmp)) {
+                    $ret = $tmp;
+                    $lastError = "";
+                }
+            } else {
+                $lastError = "{$url} no devolvió JSON";
+            }
+        } else {
+            $reasonPhrase = $response->statusText();
+            $lastError = "{$url} falló. Status code: {$statusCode} - {$reasonPhrase}";
+        }
+    } catch (\Illuminate\Http\Client\RequestException $e) {
+        $lastError = $e->getMessage();
+    } catch (\Throwable $th) {
+        $lastError = $th->getMessage();
+    }
+
+    if ($lastError != '') {
+        $ret = [
+            'resultado' => 'nok',
+            'mensaje' => $lastError,
+        ];
+    }
+
+    return empty($ret) ? [] : $ret;
+}
+
         //$dataFinal son los parámetros que se envian a la API
 
         /**Aqui ya tengo que implementar la copia de lo que hace la funcion del codigo del doc de la API original */
@@ -137,11 +187,6 @@ class EditPedido extends EditRecord
          * que necesito enviar al API
          * la otra donde realmente llame al api como en el DOC
          */
-       
-
-
-
-
         /**Todo esto es la peticion, de momento me paro para ir probando lo del token
          * 
   
@@ -173,7 +218,5 @@ class EditPedido extends EditRecord
          * 
          * 
          */
-    }    //en el record tengo lo que son todos los datos de ese momento 
-    //record es la instancia de pedido que hay 
 
 }
