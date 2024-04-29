130148e71ddedc90940d5b73d5c6585e5d0623df
BarbaraCrLp <barbaracrlp99@gmail.com>
2024-04-29 12:22:50 +0200

    La verifiacion de la contraseña como tal funciona pero no me redirige ni inicia sesion,necesito acabar de implementar bien el login

--- app/Filament/Pages/Auth/LoginEdit.php
+++ app/Filament/Pages/Auth/LoginEdit.php
@@ -17,12 +17,12 @@ use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
 class LoginEdit extends BaseAuth
 {
 
-//el nuevo formulario
+    //el nuevo formulario
     public function form(Form $form): Form
     {
         return $form
             ->schema([
-                $this->getEmailFormComponent(),  
+                $this->getEmailFormComponent(),
                 $this->getPasswordFormComponent(),
                 $this->getRememberFormComponent(),
             ])
@@ -39,7 +39,7 @@ class LoginEdit extends BaseAuth
             ->autocomplete()
             ->autofocus()
             ->extraInputAttributes(['tabindex' => 1]);
-    } 
+    }
 
     protected function getPasswordFormComponent(): Component
     {
@@ -77,48 +77,88 @@ class LoginEdit extends BaseAuth
     //hace falta implementar el metodo check y validate de la contraseña origina de arcadina
 
     public function authenticate(): ?LoginResponse
-{
-    try {
-        $this->rateLimit(5);
-    } catch (TooManyRequestsException $exception) {
-        // Handle rate limiting exception
-        return null;
+    {
+        try {
+            $this->rateLimit(5);
+        } catch (TooManyRequestsException $exception) {
+            // Handle rate limiting exception
+            return null;
+        }
+
+        $data = $this->form->getState();
+
+        // aqui esta la info que se introduce en el formulario 
+
+        $email = $data['mail'];
+        $password = $data['pass'];
+        error_log('email es ' . $email);
+        error_log('password es ' . $password);
+
+
+
+        // Retrieve the user record from the database based on the email
+        $user = Usuario::where('mail', $email)->first();
+
+        $contraseña = $user->pass;
+        error_log('contraseña del usuario ' . $contraseña);
+
+        //llamada a la funcion de la contraseña
+
+        //aqui necesito tener en una variable la contraseña del usuario 
+
+
+        // If the user doesn't exist or the password doesn't match, throw a validation exception
+
+        //creo primero la funcion y luego lo que hago es cambiar el nombre en esta condicion de aqui luego
+        if (!$user || !self::verificaContraseña($contraseña, $password)) {
+            $this->throwFailureValidationException();
+        }
+
+        // Optionally, you can perform additional checks or actions here if needed
+
+        // If authentication is successful, regenerate the session
+        session()->regenerate();
+
+        return app(LoginResponse::class);
     }
 
-    $data = $this->form->getState();
+    /**a partir de aqui la verificacion de las contraseñas
+     * tengo en una variable $data[pass]
+     * en la otra tengo $user->pass 
+     */
+    const METHOD = PASSWORD_BCRYPT;
+    public function verificaContraseña($pass_db, $pass_login): bool
+    {
 
-    // aqui esta la info que se introduce en el formulario 
+        //asi es el original pero lo convierto en constante
+        //const METHOD = PASSWORD_BCRYPT;
+        error_log('verifico quese creo como toca ');
+        if (!self::check($pass_db)) {
+            return FALSE;
+        }
 
-    $email = $data['mail'];
-    $password=$data['pass'];
-    error_log('email es '.$email);
-    error_log('password es '.$password);
+        // Quitamos la marca
+        $pass_db = substr($pass_db, 5);
 
+        // Descodificamos de base64
+        $pass_db = base64_decode($pass_db);
 
-
-    // Retrieve the user record from the database based on the email
-    $user = Usuario::where('mail', $email)->first();
-
-    $contraseña=$user->pass;
-    error_log('contraseña del usuario '.$contraseña);
-
-    //llamada a la funcion de la contraseña
-
-    //aqui necesito tener en una variable la contraseña del usuario 
-
-
-    // If the user doesn't exist or the password doesn't match, throw a validation exception
-    if (!$user || !password_verify($data['password'], $user->password)) {
-        $this->throwFailureValidationException();
+        //aqui en teoria se verifica si es o no a contraseña que toca
+        error_log('la verificacion devolveria: ' . password_verify($pass_login, $pass_db));
+        return password_verify($pass_login, $pass_db);
     }
 
-    // Optionally, you can perform additional checks or actions here if needed
+    /**otra funcion la llamada check en el original */
+    public static function check($hash)
+    {
 
-    // If authentication is successful, regenerate the session
-    session()->regenerate();
-
-    return app(LoginResponse::class);
-}
-
-
+        error_log('llama al check con el hash');
+        $ret = mb_substr($hash, 0, 5) === 'hash:';
+        if ($ret) {
+            // Verificamos que la cadena ha sido cifrada con el METHOD
+            $hashInfo = password_get_info(base64_decode(mb_substr($hash, 5)));
+            $ret = $hashInfo['algo'] == self::METHOD;
+        }
+        return $ret;
+    }
 }
