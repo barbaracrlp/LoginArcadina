a12129c389c127602a2d589adc2b5f6c5047a489
BarbaraCrLp <barbaracrlp99@gmail.com>
2024-05-06 12:58:21 +0200

    cambio del titulo del dashboard

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,11 @@
+<?php
+ 
+namespace App\Filament\Pages;
+ 
+class Dashboard extends \Filament\Pages\Dashboard
+{
+    // ...
+
+    protected static ?string $navigationLabel = 'Home';
+    protected static ?string $title = 'Home';
+}
