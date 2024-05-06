537a01ea5353ade3d3417d600860878978243d67
BarbaraCrLp <barbaracrlp99@gmail.com>
2024-05-06 10:16:01 +0200

    cambio otra vez el nombre del panel

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,11 @@
+<?php
+ 
+namespace App\Filament\Pages;
+ 
+class Dashboard extends \Filament\Pages\Dashboard
+{
+    // ...
+
+    protected static ?string $title='General';
+    protected static ?string $navigationLabel= 'General';
+}
