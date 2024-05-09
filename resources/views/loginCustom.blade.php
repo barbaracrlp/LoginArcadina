c2cdc16e70e4f75eb2a87c0c6f5da1a13cd6d656
BarbaraCrLp <barbaracrlp99@gmail.com>
2024-05-09 10:26:50 +0200

    ya esta centrado, voy a ver si ahora cambio un poco el tamaño

--- resources/views/loginCustom.blade.php
+++ resources/views/loginCustom.blade.php
@@ -59,8 +59,10 @@
 
     .formul {
         display: flex;
+        flex-direction: column;
         align-items: center;
         justify-content: center;
+        height: 100%;
     }
 
     @media screen and (max-width:800px) {
@@ -88,49 +90,37 @@
 
     <div class="flex-1 flex flex-col items-center" style="bject-fit: cover">
         <div class="formul">
-      
             <header class="fi-simple-header flex flex-col items-center" style="margin-bottom: 10px">
                 @if ($logo)
-                <x-filament-panels::logo class="mb-4" />
+                    <x-filament-panels::logo class="mb-4" />
                 @endif
-
-
-                <h1
-                    class="fi-simple-header-heading text-center text-2xl font-bold tracking-tight text-gray-950 dark:text-white">
+        
+                <h1 class="fi-simple-header-heading text-center text-2xl font-bold tracking-tight text-gray-950 dark:text-white">
                     Inicia Sesión
                 </h1>
-
-
-
+        
                 <p class="fi-simple-header-subheading mt-2 text-center text-sm text-gray-500 dark:text-gray-400">
                     Bienvenido de nuevo!
                 </p>
-
             </header>
+        
             @if (filament()->hasRegistration())
-            <x-slot name="subheading">
-                {{ __('filament-panels::pages/auth/login.actions.register.before') }}
-
-                {{ $this->registerAction }}
-            </x-slot>
+                <x-slot name="subheading">
+                    {{ __('filament-panels::pages/auth/login.actions.register.before') }}
+                    {{ $this->registerAction }}
+                </x-slot>
             @endif
-
-            {{
-            \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::AUTH_LOGIN_FORM_BEFORE,
-            scopes: $this->getRenderHookScopes()) }}
-
-
+        
+            {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::AUTH_LOGIN_FORM_BEFORE, scopes: $this->getRenderHookScopes()) }}
+        
             <x-filament-panels::form wire:submit="authenticate">
                 {{ $this->form }}
-
-                <x-filament-panels::form.actions :actions="$this->getCachedFormActions()"
-                    :full-width="$this->hasFullWidthFormActions()" />
+                <x-filament-panels::form.actions :actions="$this->getCachedFormActions()" :full-width="$this->hasFullWidthFormActions()" />
             </x-filament-panels::form>
-
-            {{
-            \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::AUTH_LOGIN_FORM_AFTER,
-            scopes: $this->getRenderHookScopes()) }}
+        
+            {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::AUTH_LOGIN_FORM_AFTER, scopes: $this->getRenderHookScopes()) }}
         </div>
+        
 
     </div>
 
