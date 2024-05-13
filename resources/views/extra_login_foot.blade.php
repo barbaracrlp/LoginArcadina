f9faed57c2a7600bb6b6a9e90926a227ad701580
BarbaraCrLp <barbaracrlp99@gmail.com>
2024-05-13 12:45:18 +0200

    ya funciona se divide la pantalla en dos y no se toca la logica de filament se añade un gradiente de color primario en caso de que no se pueda cargar la imagen funciona bien l entrar

--- resources/views/extra_login_foot.blade.php
+++ resources/views/extra_login_foot.blade.php
@@ -2,6 +2,8 @@
 @if(request()->routeIs('filament.gestion.auth.login'))
 
 </div>
+
+{{-- y este es el que tiene la foto como fondo si la foto no carga he puesto un gradiente de color primario --}}
 <div class="right-side">
     
 </div>
--- resources/views/extra_login_head.blade.php
+++ resources/views/extra_login_head.blade.php
@@ -1,37 +1,45 @@
 {{-- antes del body, crear una division que divida el body en dos --}}
 @if(request()->routeIs('filament.gestion.auth.login'))
+{{-- No tocar esto, sino toda la pagina del login cambia --}}
 <style>
-    body
-    {
+    body {
         display: flex;
-        background-color:white !important;
+        background-color: white !important;
     }
 
     /*dividir el divgeneral en dos mitades iguales flex:1 que ocupen el mismo espacio*/
-    .left-side,.right-side
-    {
-        flex:1;
+    .left-side,
+    .right-side {
+        flex: 1;
         box-sizing: border-box;
     }
 
-    .right-side
-    {
-        background-image: url('/images/fotoLogin.jpg'),linear-gradient(#3AB7E6,#0F79AE);
+    .right-side {
+        background-image: url('/images/fotoLogin.jpg'), linear-gradient(#3AB7E6, #0F79AE);
         background-size: cover;
         background-position: center;
-       
+
     }
 
-    @media screen and (max-width:800px){
-        .right-side{
-            display:none;
+    @media screen and (max-width:800px) {
+        .right-side {
+            display: none;
         }
     }
 
-    .fi-simple-main{
-        border: 0ch;
+    .fi-simple-main {
+        border-bottom-left-radius: 0%;
+        border-bottom-right-radius: 0%;
+        border-top-left-radius: 0%;
+        border-top-right-radius: 0%;
+        border: none;
+        box-shadow: none;
+        outline: none;
+        background-color: transparent;
     }
-    </style>
+</style>
+{{-- ahora creo la primera division para meter todo lo de filament en un lado
+en la otra pagina cierror este div y añado el lado derecho --}}
 
 <div class="left-side">
 @endif
