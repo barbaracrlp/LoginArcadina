99075d18d98ba6a36747d5a06f88493c6f9c3b93
BarbaraCrLp <barbaracrlp99@gmail.com>
2024-05-16 12:56:05 +0200

    Nueva main ya se ha arreglado todo, se puede volver a entrar

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -70,6 +70,11 @@ class Cliente extends Model
         $this->attributes['telefono'] = ($value !== null) ? $value : '';
     }
 
+    public function setDireccionAttribute($value)
+    {
+        $this->attributes['direccion'] = ($value !== null) ? $value : '';
+    }
+
     /**voy a probar primero la de getpassatt a ver si así lo hace automatico y en la app misma
      * la coje asi, tengo que mirar si existe un metodo de set para hashear y cambiar
      */
