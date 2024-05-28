0da88383ce7f40aef1ba9d55f38233cdc11a8f1b
BarbaraCrLp <barbaracrlp99@gmail.com>
2024-05-28 13:08:42 +0200

    quito la etiqueta

--- app/Filament/Resources/PedidoResource.php
+++ app/Filament/Resources/PedidoResource.php
@@ -365,7 +365,7 @@ class PedidoResource extends Resource
     {
         return [
 
-            EtiquetasRelationManager::class,
+            // EtiquetasRelationManager::class,
         ];
     }
 
